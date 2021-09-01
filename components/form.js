146d9bba3aeff2a7b4c00b82ff00eb8ea3cf6a9c
ASgmbv <alimbeksagymbaev@gmail.com>
2021-09-02 03:55:00 +0600

    added contacts form

---
+++ components/form.js
@@ -0,0 +1,88 @@
+import {
+	Box,
+	Button,
+	Container,
+	Flex,
+	FormControl,
+	FormLabel,
+	Grid,
+	Heading,
+	Input,
+	Stack,
+	Text,
+	Textarea,
+} from "@chakra-ui/react";
+
+const Form = () => {
+	return (
+		<Box>
+			<Grid templateColumns={["1fr", null, null, "repeat(3, 1fr)"]}>
+				<Box
+					gridColumn={[null, null, null, "1 / 3"]}
+					bg="#283646"
+					paddingY="20"
+				>
+					<Container>
+						<Stack spacing="8">
+							<Heading color="white" size="2xl">
+								Get in Touch
+							</Heading>
+							<Text color="#A2A9B0">
+								Have a project in mind that you think we’d be a great
+								fit for it? We’d love to know what you’re thinking
+							</Text>
+							<Stack direction="row" spacing="6">
+								<FormControl>
+									<FormLabel color="white" fontWeight="bold">
+										Full Name
+									</FormLabel>
+									<Input
+										borderRadius="0"
+										size="lg"
+										placeholder="Input your full name in here"
+									/>
+								</FormControl>
+								<FormControl>
+									<FormLabel color="white" fontWeight="bold">
+										Email Address
+									</FormLabel>
+									<Input
+										borderRadius="0"
+										size="lg"
+										placeholder="Input your full name in here"
+									/>
+								</FormControl>
+							</Stack>
+							<FormControl>
+								<FormLabel color="white" fontWeight="bold">
+									Email Address
+								</FormLabel>
+								<Textarea
+									borderRadius="0"
+									placeholder="Write your messages in here"
+									noOfLines="10"
+								/>
+							</FormControl>
+							<Button
+								borderRadius="0"
+								width="fit-content"
+								bg="#3A86FF"
+								color="white"
+								size="lg"
+							>
+								Send Messages
+							</Button>
+						</Stack>
+					</Container>
+				</Box>
+				<Box
+					backgroundImage="/work3.jpeg"
+					backgroundSize="cover"
+					display={["none", null, null, "block"]}
+				></Box>
+			</Grid>
+		</Box>
+	);
+};
+
+export default Form;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Box } from "@chakra-ui/react";
 import Features from "../components/features";
 import Footer from "../components/footer";
+import Form from "../components/form";
 import BannerSlider from "../components/slider";
 import { Testimonials } from "../components/testimonials";
 import Works from "../components/works";
@@ -14,8 +15,8 @@ export default function Home() {
 			<Box height="200px"></Box>
 			<Works />
 			<Box height="200px"></Box>
-
 			<Testimonials />
+			<Form />
 			<Footer />
 		</Box>
 	);
